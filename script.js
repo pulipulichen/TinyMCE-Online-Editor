673da76cf6a4ae2de43320ef11157f3c3f74a4bd
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-05-18 22:39:46 +0800

    0518 ok搞定語言設定

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ init_tinymce = function () {
 tinymce.init({
   selector: 'textarea',
   /*height: 500,*/
-  //language: 'zh_TW',
+  language: 'zh_TW',
   theme: 'modern',
   resize: false,
   style_formats: [
